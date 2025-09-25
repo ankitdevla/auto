026836fd4e407a077f1af1abcf1ded0fc07e3bd4
AnkitDevla <ankitdevla123@gmail.com>
2025-09-25 14:48:51 +0530

    2e

--- conftest.py
+++ conftest.py
@@ -12,10 +12,16 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # Suppress TensorFlow logs
 
 @pytest.fixture(scope="session")
 def setup():
-    chrome_options = Options()
-    chrome_options.add_argument("--incognito")
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    opts = Options()
+    opts.add_argument("--headless")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+    # chrome_options = Options()
+    # chrome_options.add_argument("--incognito")
+    # service = Service(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
     print("WebDriver initialized successfully.")
     yield driver    
